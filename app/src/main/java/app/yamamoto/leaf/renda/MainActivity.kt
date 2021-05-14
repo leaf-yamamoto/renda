2b13c09c6c77c9154c29beb7fd4d5f4eed611653
leaf-yamamoto <sanaeyamamoto@icloud.com>
2021-05-14 14:33:25 +0900

    変更した内容をメモ

--- app/src/main/java/app/yamamoto/leaf/renda/MainActivity.kt
+++ app/src/main/java/app/yamamoto/leaf/renda/MainActivity.kt
@@ -1,25 +1,54 @@
 package app.yamamoto.leaf.renda
 
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var second Int: = 10
     var tapCount = 0
+
+    val timer: CountDownTimer = object : CountDownTimer(10000, 10000) {
+        override fun onFinish() {
+            startButton.isVisible = true
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle_glay)
+            second = 10
+            tapCount = 0
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle)
+            second -= 1
+            secondText.text = second.toString()
+
+        }
+
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tapButton.setOnClickListener{
-
-            tapCount = tapCount +1
+        startButton.setOnClickListener {
 
             countText.text = tapCount.toString()
+            startButton.isVisible = false
 
+            timer.start()
         }
 
+        tapButton.setOnClickListener {
+
+            if (second < 10) {
+
+                tapCount = tapCount + 1
+                countText.text = tapCount.toString()
+
+            }
+        }
     }
 }
